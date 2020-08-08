4785ea2e63e4c59f2293485d0dc7316c67e6dafb
joshhannaford <hannaford.joshua55@gmail.com>
2020-08-08 16:02:51 -0400

    Basic Carbon support

---
+++ src/carbon.d.ts
@@ -0,0 +1,141 @@
+declare module '@carbon/vue' {
+
+  import { Vue } from 'vue-property-decorator'
+
+  export class CvAccordion extends Vue {}
+  export class CvAccordionItem extends Vue {}
+  export class CvAccordionSkeleton extends Vue {}
+
+  export class CvBreadcrumb extends Vue {}
+  export class CvBreadcrumbItem extends Vue {}
+  export class CvBreadcrumbSkeleton extends Vue {}
+
+  export class CvButton extends Vue {}
+  export class CvIconButton extends Vue {}
+  export class CvButtonSkeleton extends Vue {}
+
+  export class CvCheckbox extends Vue {}
+  export class CvCheckboxSkeleton extends Vue {}
+
+  export class CvCodeSnippet extends Vue {}
+  export class CvCodeSnippetSkeleton extends Vue {}
+
+  export class CvComboBox extends Vue {}
+
+  export class CvContentSwitcher extends Vue {}
+  export class CvContentSwitcherButton extends Vue {}
+  export class CvContentSwitcherContent extends Vue {}
+
+  export class CvDataTable extends Vue {}
+  export class CvDataTableRow extends Vue {}
+  export class CvDataTableCell extends Vue {}
+  export class CvDataTableAction extends Vue {}
+  export class CvDataTableSkeleton extends Vue {}
+
+  export class CvDatePicker extends Vue {}
+
+  export class CvDropdown extends Vue {}
+  export class CvDropdownItem extends Vue {}
+  export class CvDropdownSkeleton extends Vue {}
+
+  export class CvFileUploader extends Vue {}
+  export class CvFileUploaderSkeleton extends Vue {}
+
+  export class CvForm extends Vue {}
+  export class CvFormItem extends Vue {}
+  export class CvFormGroup extends Vue {}
+
+  export class CvInlineLoading extends Vue {}
+  export class CvInlineNotification extends Vue {}
+
+  export class CvLink extends Vue {}
+
+  export class CvList extends Vue {}
+  export class CvListItem extends Vue {}
+
+  export class CvLoading extends Vue {}
+
+  export class CvModal extends Vue {}
+
+  export class CvMultiSelect extends Vue {}
+
+  export class CvNumberInput extends Vue {}
+  export class CvNumberInputSkeleton extends Vue {}
+
+  export class CvOverflowMenu extends Vue {}
+  export class CvOverflowMenuItem extends Vue {}
+
+  export class CvPagination extends Vue {}
+
+  export class CvProgress extends Vue {}
+  export class CvProgressStep extends Vue {}
+
+  export class CvRadioGroup extends Vue {}
+  export class CvRadioButton extends Vue {}
+
+  export class CvSearch extends Vue {}
+
+  export class CvSelect extends Vue {}
+  export class CvSelectOption extends Vue {}
+  export class CvSelectOptgroup extends Vue {}
+
+  export class CvSkeletonText extends Vue {}
+
+  export class CvSlider extends Vue {}
+  export class CvSliderSkeleton extends Vue {}
+
+  export class CvStructuredList extends Vue {}
+  export class CvStructuredListItem extends Vue {}
+  export class CvStructuredListHeading extends Vue {}
+  export class CvStructuredListData extends Vue {}
+
+  export class CvTab extends Vue {}
+  export class CvTabs extends Vue {}
+  export class CvTabsSkeleton extends Vue {}
+
+  export class CvTag extends Vue {}
+  export class CvTagSkeleton extends Vue {}
+
+  export class CvTextArea extends Vue {}
+
+  export class CvTextInput extends Vue {}
+
+  export class CvTile extends Vue {}
+
+  export class CvTimePicker extends Vue {}
+
+  export class CvToastNotification extends Vue {}
+
+  export class CvToggle extends Vue {}
+
+  export class CvToolbar extends Vue {}
+  export class CvToolbarTitle extends Vue {}
+  export class CvToolbarSearch extends Vue {}
+  export class CvToolbarOption extends Vue {}
+  export class CvToolbarDivider extends Vue {}
+
+  export class CvTooltip extends Vue {}
+  export class CvInteractiveTooltip extends Vue {}
+  export class CvDefinitionTooltip extends Vue {}
+
+  export class CvContent extends Vue {}
+  export class CvHeaderGlobalAction extends Vue {}
+  export class CvHeaderMenuButton extends Vue {}
+  export class CvHeaderMenuItem extends Vue {}
+  export class CvHeaderMenu extends Vue {}
+  export class CvHeaderName extends Vue {}
+  export class CvHeaderPanel extends Vue {}
+  export class CvHeaderNav extends Vue {}
+  export class CvHeader extends Vue {}
+  export class CvSideNav extends Vue {}
+  export class CvSideNavIcon extends Vue {}
+  export class CvSideNavItems extends Vue {}
+  export class CvSideNavLink extends Vue {}
+  export class CvSideNavMenuItem extends Vue {}
+  export class CvSideNavMenu extends Vue {}
+  export class CvSkipToContent extends Vue {}
+  export class CvSwitcherItemLink extends Vue {}
+  export class CvSwitcherItem extends Vue {}
+  export class CvSwitcher extends Vue {}
+
+}
